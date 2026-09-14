594768f334aba88416a49d12500ace41aa779892
ChristianC720 <170275348+ChristianC720@users.noreply.github.com>
2026-09-14 16:18:12 -0500

    feature: se agrega la consulta de Videojuegos

--- src/controllers/videojuegos.controller.js
+++ src/controllers/videojuegos.controller.js
@@ -1,6 +1,8 @@
-import { id, query } from '../validators/query.validator.js';
+import { id, query, idempotencyKey } from '../validators/query.validator.js';
 export function createVideojuegosController(service) {
   return {
+    list: async (req, res) => res.json(await service.list(query(req.query, []))),
+    get: async (req, res) => res.json({ data: await service.get(id(req.params.id)) }),
     create: async (req, res) => {
       res.status(201).json({ data: await service.create(req.body) });
     },
--- src/services/videojuegos.service.js
+++ src/services/videojuegos.service.js
@@ -6,6 +6,8 @@ export class VideojuegosService {
     const data = await this.repository.list(options);
     return { data, pagination: { page: options.page, limit: options.limit } };
   }
+  get(id) { return this.repository.get(id); }
   create(input) { return this.repository.create(validarVideojuegos(input)); }
+  update(id, input) { return this.repository.update(id, validarVideojuegos(input, true)); }
   remove(id) { return this.repository.remove(id); }
 }
